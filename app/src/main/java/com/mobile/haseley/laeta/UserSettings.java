793655f9b7004d8e283e6bd60fd2c4bc1ad1944e
abbyaha <haseley.3@osu.edu>
2016-06-09 21:20:50 -0400

    made file for user settings...literally nothing else but im playing with new shell so i wanted to do something

---
+++ app/src/main/java/com/mobile/haseley/laeta/UserSettings.java
@@ -0,0 +1,18 @@
+package com.mobile.haseley.laeta;
+
+import android.support.design.widget.NavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+/**
+ * Created by Abby on 5/21/16.
+ * All the functionality for creating and editing a user profile.
+ */
+public class UserSettings extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener{
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        return false;
+    }
+}
